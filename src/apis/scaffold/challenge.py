d7c0fbf549723f872184b5fbb908c78773d9338e
shah77pc <73152402+shah77pc@users.noreply.github.com>
2022-05-29 00:18:45 -0700

    Update challenge.py

--- src/apis/scaffold/challenge.py
+++ src/apis/scaffold/challenge.py
@@ -5,6 +5,10 @@
 # Description:
 import time
 from typing import Optional
+import pymongo
+from pymongo import MongoClient
+import datetime
+import pprint
 
 from selenium.common.exceptions import WebDriverException
 
@@ -12,70 +16,104 @@ from services.hcaptcha_challenger import ArmorCaptcha, ArmorUtils
 from services.hcaptcha_challenger.exceptions import ChallengePassed
 from services.settings import logger, HCAPTCHA_DEMO_SITES, DIR_MODEL, DIR_CHALLENGE
 from services.utils import get_challenge_ctx
-
+busy = False
 
 @logger.catch()
+def goto(linenum):
+    global line
+    line = linenum
+
 def runner(
     sample_site: str,
     lang: Optional[str] = "zh",
     silence: Optional[bool] = False,
     onnx_prefix: Optional[str] = None,
 ):
-    """Human-Machine Challenge Demonstration | Top Interface"""
     logger.info("Starting demo project...")
-
-    # Instantiating Challenger Components
+    """Human-Machine Challenge Demonstration | Top Interface"""
     challenger = ArmorCaptcha(dir_workspace=DIR_CHALLENGE, lang=lang, debug=True)
     challenger_utils = ArmorUtils()
 
     # Instantiating the Challenger Drive
     ctx = get_challenge_ctx(silence=silence, lang=lang)
-    try:
-        for i in range(5):
-            try:
-                # Read the hCaptcha challenge test site
-                ctx.get(sample_site)
+    logger.info("Starting demo project...")
+    client = MongoClient("mongodb://20.118.216.29:27018")
+    mydatabase = client['1']
+    mydatabase2 = client['4']
+    db = client["responses"]
+    #sitekey1={"12345678":"1", "date":datetime.datetime.utcnow()}
+      #mydatabase.sitekey.insert_one(sitekey1)
+      #mydatabase.create_collection("response")
+      #mydatabase.create_collection('sitekey')
+    tasklist=mydatabase.res.count_documents({})
+    while (tasklist==0):
+        tasklist=mydatabase.res.count_documents({})
 
-                # Detects if a clickable `hcaptcha checkbox` appears on the current page.
-                # The `sample site` must pop up the `checkbox`, where the flexible wait time defaults to 5s.
-                # If the `checkbox` does not load in 5s, your network is in a bad state.
-                if not challenger_utils.face_the_checkbox(ctx):
-                    break
-                start = time.time()
+        print(tasklist)
+  
+    ai_task=mydatabase.res.find_one_and_delete({})
+    print(ai_task)
+    print(ai_task['2'])
+    object_id=ai_task['_id']
+    if (ai_task['2']=="2cae9d15-bde9-4a43-9e2a-5f4a1578d40b"):
+      for i in range(20):
+          if i!=0 :
+             if (token!="") and i!=0:
+                   break
+          ctx.get("http://20.118.216.29/demo1")
 
-                # Enter iframe-checkbox --> Process hcaptcha checkbox --> Exit iframe-checkbox
-                challenger.anti_checkbox(ctx)
+          challenger.anti_checkbox(ctx)
 
                 # Enter iframe-content --> process hcaptcha challenge --> exit iframe-content
-                resp = challenger.anti_hcaptcha(ctx, dir_model=DIR_MODEL, onnx_prefix=onnx_prefix)
-                if resp == challenger.CHALLENGE_SUCCESS:
-                    challenger.log(f"End of demo - total: {round(time.time() - start, 2)}s")
-                    logger.success(f"PASS[{i + 1}|5]".center(28, "="))
-                elif resp == challenger.CHALLENGE_RETRY:
+
+          resp = challenger.anti_hcaptcha(ctx, dir_model=DIR_MODEL, onnx_prefix=onnx_prefix)
+          token = ctx.execute_script("return hcaptcha.getResponse();")
+         # while (token==""):
+            #  challenger.anti_checkbox(ctx)
+
+                # Enter iframe-content --> process hcaptcha challenge --> exit iframe-content
+
+            #  resp = challenger.anti_hcaptcha(ctx, dir_model=DIR_MODEL, onnx_prefix=onnx_prefix)
+            #  token = ctx.execute_script("return hcaptcha.getResponse();")
+            
+          if resp == challenger.CHALLENGE_SUCCESS and token != "":
+                    token = ctx.execute_script("return hcaptcha.getResponse();")
+                    sitekey1={'_id': object_id, '6': '2cae9d15-bde9-4a43-9e2a-5f4a1578d40b', '5': token, 'groups': []}
+                    mydatabase2.res.insert_one(sitekey1)
+                    print ("done!")
+          elif resp == challenger.CHALLENGE_RETRY or token=="":
                     ctx.refresh()
+                    goto(55)
+    
+    #go to http://20.118.216.29/demo1 
+    # Instantiating Challenger Components
+    if (ai_task['2']=="6ba78ccd-f275-4c2d-be45-5e4b46a4a4d8"):
+      for i in range(20):
+          if i!=0:
+              if (token!="") and i!=0:
+                   break
 
-            # Do not capture the `ChallengeReset` signal in the outermost layer.
-            # In the demo project, we wanted the human challenge to pop up, not pass after processing the checkbox.
-            # So when this happens, we reload the page to activate hcaptcha repeatedly.
-            # But in your project, if you've passed the challenge by just handling the checkbox,
-            # there's no need to refresh the page!
-            except ChallengePassed:
-                ctx.refresh()
-            except WebDriverException as err:
-                logger.exception(err)
-    finally:
-        print("[EXIT] Press any key to exit...")
+          ctx.get("http://20.118.216.29/demo2")
 
-        ctx.quit()
+          challenger.anti_checkbox(ctx)
 
+                # Enter iframe-content --> process hcaptcha challenge --> exit iframe-content
 
-@logger.catch()
-def test():
-    """Check if the Challenger driver version is compatible"""
-    ctx = get_challenge_ctx(silence=True)
-    try:
-        ctx.get(HCAPTCHA_DEMO_SITES[0])
-    finally:
-        ctx.quit()
+          resp = challenger.anti_hcaptcha(ctx, dir_model=DIR_MODEL, onnx_prefix=onnx_prefix)
+          token = ctx.execute_script("return hcaptcha.getResponse();")
+          #while (token==""):
+            #  challenger.anti_checkbox(ctx)
 
-    logger.success("The adaptation is successful")
+                # Enter iframe-content --> process hcaptcha challenge --> exit iframe-content
+
+            #  resp = challenger.anti_hcaptcha(ctx, dir_model=DIR_MODEL, onnx_prefix=onnx_prefix)
+             # token = ctx.execute_script("return hcaptcha.getResponse();")
+             
+          if resp == challenger.CHALLENGE_SUCCESS and token != "":
+                    token = ctx.execute_script("return hcaptcha.getResponse();")
+                    sitekey1={'_id': object_id, '6': '6ba78ccd-f275-4c2d-be45-5e4b46a4a4d8', '5': token, 'groups': []}
+                    mydatabase2.res.insert_one(sitekey1)
+                    print ("done!")
+          elif resp == challenger.CHALLENGE_RETRY or token == "":
+                    ctx.refresh()
+                    goto(83)
